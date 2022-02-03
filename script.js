12af5084e3958ac7b5365b2fa09fb8291d95fa06
Bryan Panaccione <bryan.j.panaccione@gmail.com>
2022-02-02 21:01:55 -0600

    "Red Checkers"

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 // Your JS goes here
-function checker() {
 
+function makeBoard() {
+    let i = 99;
+    while (i > 0) {
+        if (i % 2 === 0) {
+            let redChecker = makeChecker();
+            redChecker.style.backgroundColor = 'red'
+            document.body.appendChild(redChecker)
+        } else {
+            let blackChecker = makeChecker();
+            blackChecker.style.backgroundColor = 'black'
+            document.body.appendChild(blackChecker)
+        } i--
+    }
 }
+
+function makeChecker() {
+    let checker = document.createElement('div');
+    checker.style.width = '11.1%';
+    checker.style.paddingBottom = '11.1%';
+    checker.style.float = 'left'
+    return checker
+}
+
+makeBoard();
